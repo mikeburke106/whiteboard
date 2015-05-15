a11924205a57fdf736d3c8b148c8c02947857cb8
Mike Burke <mike@livioconnect.com>
2015-05-15 19:59:42 -0400

    initial implementation of custom whiteboard view

--- app/src/main/java/com/burkeapps/whiteboard/MainActivity.java
+++ app/src/main/java/com/burkeapps/whiteboard/MainActivity.java
@@ -1,36 +1,41 @@
 package com.burkeapps.whiteboard;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.burkeapps.whiteboard.views.WhiteboardView;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    WhiteboardView whiteboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        whiteboard = (WhiteboardView) findViewById(R.id.whiteboard);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(id){
+            case R.id.action_erase:
+                whiteboard.erase();
+                return true;
+            default:
+                break;
         }
 
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/burkeapps/whiteboard/views/WhiteboardView.java
+++ app/src/main/java/com/burkeapps/whiteboard/views/WhiteboardView.java
@@ -1,14 +1,29 @@
 package com.burkeapps.whiteboard.views;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
  * TODO: document your custom view class.
  */
 public class WhiteboardView extends View {
+
+    // TODO: Read these values from resources
+    private static int DEFAULT_PAINT_COLOR = Color.BLACK;
+    private static int DEFAULT_STROKE_WIDTH = 30;
+
+    Path touchPath;
+    Paint touchPaint, canvasPaint;
+    Bitmap canvasBitmap;
+    Canvas touchCanvas;
+    int canvasHeight, canvasWidth;
 
     public WhiteboardView(Context context) {
         super(context);
@@ -28,12 +43,101 @@ public class WhiteboardView extends View {
     private void init(AttributeSet attrs, int defStyle) {
         // Load attributes
 
+        // lazily instantiate our objects
+        initTouchPaint();
+        initCanvasPaint();
+        initTouchPath();
+    }
+
+    private void initTouchPaint(){
+        if(touchPaint == null) {
+            touchPaint = new Paint();
+            touchPaint.setAntiAlias(true);
+            touchPaint.setColor(DEFAULT_PAINT_COLOR);
+            touchPaint.setStyle(Paint.Style.STROKE);
+            touchPaint.setStrokeWidth(DEFAULT_STROKE_WIDTH);
+        }
+    }
+
+    private void initCanvasPaint() {
+        if(canvasPaint == null) {
+            canvasPaint = new Paint();
+        }
+    }
+
+    private void initTouchPath() {
+        if(touchPath == null) {
+            touchPath = new Path();
+        }
+
+        // ensure path is in an empty state
+        touchPath.reset();
+    }
+
+    private void initCanvas(){
+        canvasBitmap = Bitmap.createBitmap(canvasWidth, canvasHeight, Bitmap.Config.ARGB_8888);
+        touchCanvas = new Canvas(canvasBitmap);
+    }
+
+    private void initCanvas(int width, int height){
+        this.canvasWidth = width;
+        this.canvasHeight = height;
+        initCanvas();
+    }
+
+    @Override
+    protected void onSizeChanged (int w, int h, int oldw, int oldh){
+        super.onSizeChanged(w,h,oldw,oldh);
+
+        // the size of the whiteboard changed, re-initialize the canvas
+        initCanvas(w, h);
+    }
+
+    @Override
+    public boolean onTouchEvent (MotionEvent event){
+        int action = event.getAction();
+        switch(action){
+            case MotionEvent.ACTION_DOWN:
+                // user touched the screen - start path at this point
+                touchPath.moveTo(event.getX(), event.getY());
+                break;
+            case MotionEvent.ACTION_UP:
+                // user released their touch, draw it on the canvas and reset the path
+                touchCanvas.drawPath(touchPath, touchPaint);
+                touchPath.reset();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                // user moved their touch, connect the two points with a line
+                touchPath.lineTo(event.getX(), event.getY());
+                break;
+            default:
+                // don't care about this touch event - tell system we didn't process it
+                return false;
+        }
+
+        // re-draw and tell system we processed this event by returning true
+        invalidate();
+        return true;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+        // draw the existing bitmap of our canvas
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        // draw the touch path over the bitmap
+        canvas.drawPath(touchPath, touchPaint);
+    }
 
+    /*
+    *  PUBLIC APIs
+    */
 
+    public void erase(){
+        // re-initialize our touch objects
+        initTouchPath();
+        initCanvas();
+
+        // re-draw the whiteboard
+        invalidate();
     }
 }
